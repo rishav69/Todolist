b672064c102c3f45b99ce97dc1dc73257af028de
unknown <rishavshrsth@gmail.com>
2024-04-07 11:38:47 +0545

    project:todo-list

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import Logo from "../assets/logo.svg"
+export const Header = () => {
+  return (
+    <header>
+        <div className="logo">
+            <img src={Logo} alt="Taskmate Logo"  />
+            <span>Taskmate</span>
+        </div>
+        <div className="themeSelector">
+            <span className="light"></span>
+            <span className="medium"></span>
+            <span className="dark"></span>
+            <span className="gOne"></span>
+            <span className="gTwo"></span>
+            <span className="gThree"></span>
+        </div>
+    </header>
+  ) 
+}
